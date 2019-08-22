8fedeb35d5a1395c7132bf860a295c58cfb75b3f
Mervin Flores <>
2019-08-22 17:06:52 -0400

    - [ADDED] Xcode empty project for app.

---
+++ FoodieApp/FoodieApp/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  FoodieApp
+//
+//  Created by Mervin Flores on 8/22/19.
+//  Copyright © 2019 Mervin Flores. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
